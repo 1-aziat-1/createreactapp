d4382a613ad7ff4999915c8dbd03a8e1b313382f
1-aziat-1 <as.ac2727@mail.ru>
2022-09-06 00:45:43 +0300

    create react app webpack

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import maks from './img/ds.png';
+
+const Module = () => (
+    <>
+        <h1>Hello, React</h1>
+        <img width={200}src={maks}/>
+    </>    
+)
+
+export default Module;
